216fc881b5fb5eb78a3ac8d1d44dcade21a0d953
Juls <bascojulian31@gmail.com>
2024-01-21 09:04:35 +0800

    Add key prop to config

--- src/app/lib/configParser.js
+++ src/app/lib/configParser.js
@@ -2,8 +2,8 @@ import { configuration } from "../configFIle/config"
 import { widgetParser } from "./widgetPaerser"
 
 export const configParser = () => {
-    let componentBox = configuration.columns.map((column) => (
-        <div className={`col-span-${column.size} column-design`} >
+    let componentBox = configuration.columns.map((column, idx) => (
+        <div key={`column.`} className={`col-span-${column.size} column-design`} >
             <h1>{column.heading}</h1>
             <>
                 {widgetParser(column.widgets)}
